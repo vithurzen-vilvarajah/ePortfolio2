e6cdf60187068515c2831ddde221d8d4b0eec65b
vithurzen <vithurzen.vilvarajah@edu.univ-paris13.fr>
2023-06-25 01:47:30 +0200

    first commit

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import '../styles/modern-normalize.css';
+import '../styles/style.css';
+import '../styles/compenents/header.css';
+import '../styles/compenents/hero.css';
+import '../styles/compenents/about.css';
+import '../styles/compenents/mobile-nav.css';
+import '../styles/utils.css';
+
+import mobileNav from './utils/mobile-nav';
+import darkMode from './utils/dark-mode';
+mobileNav();
+darkMode();
